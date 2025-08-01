8a24368c5bac3db43e6945a6772a0421daeb4f0a
ashwinii <ashwinipowar2111@gmail.com>
2025-08-01 15:53:38 +0530

    Update largest_no._in_array.py

--- largest_no._in_array.py
+++ largest_no._in_array.py
@@ -27,6 +27,29 @@ print(largest)
 
 
 
+
+
+#second largest no.
+nums = [22, 3, 5, 6, 666, 88, 99, 0]
+
+largest = float('-inf')
+second_largest = float('-inf')
+
+for i in range(len(nums)):
+    if nums[i] > largest:
+        second_largest = largest
+        largest = nums[i]
+    elif nums[i] > second_largest and nums[i] != largest:
+        second_largest = nums[i]
+
+print(second_largest)
+
+
+
+
+
+
+
 #method easy
 def find_largest(nums):
     largest = nums[0]
